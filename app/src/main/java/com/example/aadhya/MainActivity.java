9a2f3c4cc7ccc89947fd4e6b501c64b71fb060ab
unknown <aishwaryababu1212@gmail.com>
2020-08-30 19:47:30 +0530

    modified login

--- app/src/main/java/com/example/aadhya/MainActivity.java
+++ app/src/main/java/com/example/aadhya/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.aadhya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = findViewById(R.id.loginButton);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
