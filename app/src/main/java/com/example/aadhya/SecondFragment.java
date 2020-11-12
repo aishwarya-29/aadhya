f8373793e0bc9952b990a75bc356744afe8f6322
unknown <aishwaryababu1212@gmail.com>
2020-11-12 11:39:14 +0530

    display about

--- app/src/main/java/com/example/aadhya/SecondFragment.java
+++ app/src/main/java/com/example/aadhya/SecondFragment.java
@@ -2,6 +2,7 @@ package com.example.aadhya;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,71 +11,55 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SecondFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class SecondFragment extends Fragment {
-//    EditText edn,edp,ede;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SecondFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SecondFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SecondFragment newInstance(String param1, String param2) {
-        SecondFragment fragment = new SecondFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    View v;
+    EditText editName, editEmail, editPhone;
+    DatabaseReference databaseReference;
+    String name, phone, currentUserEmail;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//         Inflate the layout for this fragment
-//        edn.findViewById(R.id.editname);
-//        edp.findViewById(R.id.editphone);
-//        ede.findViewById(R.id.editemail);
-//        edn.setEnabled(true);
-//        edp.setEnabled(true);
-//        ede.setEnabled(true);
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        v = inflater.inflate(R.layout.fragment_second, container, false);
+        editName = v.findViewById(R.id.editname);
+        editEmail = v.findViewById(R.id.editemail);
+        editPhone = v.findViewById(R.id.editphone);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        currentUserEmail = currentUser.getEmail();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("User").orderByChild("uemail").equalTo(currentUserEmail).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds : snapshot.getChildren()) {
+                    name = ds.child("uname").getValue(String.class);
+                    phone = ds.child("umobno").getValue(String.class);
+                    setDetails();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        return v;
     }
-//    public void editAbout(LayoutInflater inflater, ViewGroup container,
-//                     Bundle savedInstanceState)
-//    {
-//        edn.setEnabled(false);
-//        edp.setEnabled(false);
-//        ede.setEnabled(false);
-//    }
+
+    private void setDetails() {
+        editName.setText(name);
+        editEmail.setText(currentUserEmail);
+        editPhone.setText(phone);
+    }
 
 }
